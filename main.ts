66e6592abd5e8eac5ec8bc162873571232510dcb
samFRcouldnttinkofausername <122544219+samFRcouldnttinkofausername@users.noreply.github.com>
2023-04-26 13:11:11 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,24 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    action = receivedNumber
+})
+let action = 0
 joystickbit.initJoystickBit()
-radio.setGroup(30)
+radio.setGroup(7)
 basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (joystickbit.getButton(joystickbit.JoystickBitPin.P12)) {
+        radio.sendNumber(5)
+    } else if (joystickbit.getButton(joystickbit.JoystickBitPin.P13)) {
+        radio.sendNumber(6)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    if (action == 5) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 60)
+    }
+    if (action == 6) {
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+    }
+})
